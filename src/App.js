a1d82622aab47d3ce9b3e2500a93e5b0e4288fc1
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-06 14:15:30 -0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
-import logo from './logo.png';
-import video from './video.mp4';
-import styled from 'styled-components';
-import ScheduleList from './Components/ScheduleList';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-const StyledApp = styled.div`
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  h1 {
-    color: #fff;
-  }
-`;
-
-const Container = styled.div`
-  background-color: rgba(0,0,0,0.8);
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 99;
-`;
-
-const StyledLogo = styled.img`
-  max-width: 500px;
-  user-select: none;
-`;
-
-const StyledVideo = styled.video`
-  object-fit: cover;
-  filter: blur(10px);
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-`
+import Login from "./Pages/login";
+import MainPage from "./Pages/main";
+import NotFoundPage from "./Pages/main/notFound";
 
 function App() {
   return (
-    <StyledApp>
-      <StyledVideo src={video} muted autoPlay loop/>
-      <Container>
-        <StyledLogo src={logo}/>
-        <h1>Site em construção</h1>
-        <ScheduleList/>
-      </Container>
-    </StyledApp>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/main/index.js
@@ -0,0 +1,51 @@
+import logo from './Assets/logo.png';
+import video from './Assets/video.mp4';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  h1 {
+    color: #fff;
+  }
+`;
+
+const Container = styled.div`
+  background-color: rgba(0,0,0,0.8);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 99;
+`;
+
+const StyledLogo = styled.img`
+  max-width: 500px;
+  user-select: none;
+`;
+
+const StyledVideo = styled.video`
+  object-fit: cover;
+  filter: blur(10px);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`
+
+function MainPage() {
+  return (
+    <StyledApp>
+      <StyledVideo src={video} muted autoPlay loop/>
+      <Container>
+        <StyledLogo src={logo}/>
+        <h1>Site em construção</h1>
+      </Container>
+    </StyledApp>
+  );
+}
+
+export default MainPage;

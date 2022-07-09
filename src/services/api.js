4f4ceff88c73d5e47010cd4e32c4df118dadc282
Chrystian Farias <chrystian.farias@outlook.com>
2022-07-09 10:42:18 -0300

    modified:   src/services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'https://sharkrunnersapi.herokuapp.com/'});
+const api = axios.create({baseURL: 'https://api.sharkrunners.com.br/'});
 export default api;
